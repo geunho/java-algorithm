8f7f0044310c0fa95db9d048c48f138263a6c66c
GeunhoKim <geunho.dev@gmail.com>
2019-10-17 18:29:31 +0900

    Solve a stack and queue problem 2

--- programmers.co.kr/src/main/java/dev/geunho/StackAndQueue.java
+++ programmers.co.kr/src/main/java/dev/geunho/StackAndQueue.java
@@ -1,7 +1,10 @@
 package dev.geunho;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class StackAndQueue {
 
@@ -37,5 +40,69 @@ public class StackAndQueue {
 
         int[] answer = days.stream().mapToInt(Integer::valueOf).toArray();
         return answer;
+    }
+
+    /**
+     * 프린터 https://programmers.co.kr/learn/courses/30/lessons/42587
+     * 
+     * @param priorities
+     * @param location
+     * @return
+     */
+    public static int 프린터(int[] priorities, int location) {
+        Queue<PriorityIndex> origin = new LinkedList<PriorityIndex>();
+
+        // 순회하면서 우선순위와 위치를 저장한 자료구조를 생성
+        for (int loc = 0; loc < priorities.length; loc++) {
+            int priority = priorities[loc];
+            PriorityIndex pi = new PriorityIndex(priority, loc);
+            origin.offer(pi);
+        }
+
+        // 내림차순 정렬한 큐(A)와 정렬하지 않은 큐(B) 생성
+        Queue<PriorityIndex> ordered = origin.stream().sorted((pi1, pi2) -> pi2.getPriority() - pi1.getPriority())
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        int order = 0;
+        // 각 큐에서 값을 꺼내서 A > B면 B는 다시 enqueue,
+        while (!origin.isEmpty()) {
+            PriorityIndex orderedPi = ordered.peek();
+            PriorityIndex originPi = origin.poll();
+
+            if (orderedPi.getPriority() > originPi.getPriority()) {
+                origin.offer(originPi);
+            } else {
+                // 아니라면 카운트를 증가시키고
+                // 해당 값의 인덱스가 location과 일치하면 카운트 반환,
+                // 아니라면 next z
+                order++;
+                ordered.remove();
+                if (originPi.getLocation() == location) {
+                    return order;
+                }
+
+            }
+        }
+
+        return order;
+    }
+
+}
+
+class PriorityIndex {
+    private int priority;
+    private int location;
+
+    public PriorityIndex(int priority, int location) {
+        this.priority = priority;
+        this.location = location;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public int getLocation() {
+        return this.location;
     }
 }
--- programmers.co.kr/src/test/java/dev/geunho/StackAndQueueTest.java
+++ programmers.co.kr/src/test/java/dev/geunho/StackAndQueueTest.java
@@ -1,6 +1,7 @@
 package dev.geunho;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -15,5 +16,25 @@ public class StackAndQueueTest {
         int[] result = StackAndQueue.기능개발(progresses, speeds);
 
         assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void 프린터_test_case_1() {
+        int[] priorities = { 2, 1, 3, 2 };
+        int location = 2;
+
+        int result = StackAndQueue.프린터(priorities, location);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void 프린터_test_case_2() {
+        int[] priorities = { 1, 1, 9, 1, 1, 1 };
+        int location = 0;
+
+        int result = StackAndQueue.프린터(priorities, location);
+
+        assertEquals(5, result);
     }
 }
